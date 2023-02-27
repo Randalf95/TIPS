4d9ec1319149845b8dc48d7b50a4354f74c1d249
Alejandro Randazzo <alejandro.randazzo@hotmail.com>
2023-02-27 14:24:39 -0300

    new commit- vnf

--- Ejercicios/01-FindNeedle/FindNeedle.js
+++ Ejercicios/01-FindNeedle/FindNeedle.js
@@ -1,6 +1,20 @@
 function FindNeedle (haystack, needle) {
   // Your code here:
-
+/* for (let i = 0; i<haystack.length; i++ ){ //react-redux
+  for(let j=0; j<needle.length; j++){ //redux
+    if (needle[j]!==haystack[i +j]){
+      break
+    }
+    if (j+1 === needle.length)
+    return i
+  }
 }
-
+return -1
+} */
+for (let i = 0; i < haystack.length; i++) {
+  if (haystack.slice(i, needle.length + i=== needle)){
+    return i
+  } 
+}
+return -1
 module.exports = FindNeedle
